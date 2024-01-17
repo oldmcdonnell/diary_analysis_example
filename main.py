def7c8910ca913e8073bd414579b4b3ffabf699a
oldmcdonnell <oldmcdonnell@gmail.com>
2024-01-17 01:10:52 -0500

    validate scores 2

--- main.py
+++ main.py
@@ -6,4 +6,19 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 
 filepaths = glob.glob("diary/*.txt")
 
-print(filepaths)
+analyzer = SentimentIntensityAnalyzer()
+
+negativity = []
+positivity = []
+for filepath in filepaths:
+    with open(filepath) as file:
+        content = file.read()
+    scores = analyzer.polarity_scores(content)
+    positivity.append(scores["pos"])
+    negativity.append(scores["neg"])
+
+dates = [name.strip(".txt").strip("diary/") for name in filepaths]
+
+print(scores)
+print(negativity)
+print(positivity)
